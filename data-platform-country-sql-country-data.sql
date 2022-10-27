93c90a574ac6aa407b97c6f08db6d02af83ee768
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-27 11:45:32 +0900

    release

--- data-platform-country-sql-country-data.sql
+++ data-platform-country-sql-country-data.sql
@@ -1,7 +1,10 @@
 CREATE TABLE `data_platform_country_country_data` (
     `Country`           varchar(3) NOT NULL,
     `GlobalRegion`      varchar(3) NOT NULL,
-PRIMARY KEY (`Country`),
-CONSTRAINT `DataPlatformCountryCountryData_fk` FOREIGN KEY (`GlobalRegion`) REFERENCES `data_platform_global_region_global_region_data` (`GlobalRegion`)
+
+  PRIMARY KEY (`Country`),
+
+  CONSTRAINT `DataPlatformCountryCountryDataGlobalRegion_fk` FOREIGN KEY (`GlobalRegion`) REFERENCES `data_platform_global_region_global_region_data` (`GlobalRegion`)
+
 ) ENGINE=InnoDB 
   DEFAULT CHARSET=utf8mb4;
--- data-platform-country-sql-country-text-data.sql
+++ data-platform-country-sql-country-text-data.sql
@@ -1,8 +1,13 @@
 CREATE TABLE `data_platform_country_country_text_data` (
+
     `Country`                    varchar(3) NOT NULL,
     `Language`                   varchar(3) NOT NULL,
-    `CountryName`                varchar(100) DEFAULT NULL,
+    `CountryName`                varchar(100) NOT NULL,
+
     PRIMARY KEY (`Country`, `Language`),
-    CONSTRAINT `DataPlatformCountryCountryTextData_fk` FOREIGN KEY (`Country`) REFERENCES `data_platform_country_country_data` (`Country`)
+
+    CONSTRAINT `DataPlatformCountryCountryTextData_fk` FOREIGN KEY (`Country`) REFERENCES `data_platform_country_country_data` (`Country`),
+    CONSTRAINT `DataPlatformCountryCountryTextDataLanguage_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
+
 ) ENGINE=InnoDB
   DEFAULT CHARSET=utf8mb4;
